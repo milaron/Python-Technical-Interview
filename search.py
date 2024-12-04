af717d67cb3fa04a7c026e15dedfc6782df0630a
Aaron Miller <milaron865@gmail.com>
2024-12-03 21:10:32 -0500

    Making updates to code

---
+++ search.py
@@ -0,0 +1,24 @@
+def linear_search(search_list, target_value):
+  for idx in range(len(search_list)):
+    if search_list[idx] == target_value:
+      return idx
+  raise ValueError("{0} not in list".format(target_value))
+
+def binary_search(sorted_list, left_pointer, right_pointer, target):
+  # this condition indicates we've reached an empty "sub-list"
+  if left_pointer >= right_pointer:
+    return "value not found"
+	
+  # We calculate the middle index from the pointers now
+  mid_idx = (left_pointer + right_pointer) // 2
+  mid_val = sorted_list[mid_idx]
+
+  if mid_val == target:
+    return mid_idx
+  if mid_val > target:
+    # we reduce the sub-list by passing in a new right_pointer
+    return binary_search(sorted_list, left_pointer, mid_idx, target)
+  if mid_val < target:
+    # we reduce the sub-list by passing in a new left_pointer
+    return binary_search(sorted_list, mid_idx + 1, right_pointer, target)
+  
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-import os
-import torch
+from node import Node
+from linkedlist import LinkedList
+from doublylinkedlist import DoublyLinkedList
+from queues import Queue
+from stack import Stack
+
+test = 1
